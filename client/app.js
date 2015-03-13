1ee32e88fd239c19e18c4b05de2a037067c8ae4f
Maor <maoryosef@gmail.com>
2015-03-13 02:26:14 +0200

    basic kinetic scroll works

--- client/app.js
+++ client/app.js
@@ -1,11 +1,22 @@
 (function(window) {
+	window.requestAnimFrame = (function(){
+          return  window.requestAnimationFrame       || 
+                  window.webkitRequestAnimationFrame || 
+                  window.mozRequestAnimationFrame    || 
+                  window.oRequestAnimationFrame      || 
+                  window.msRequestAnimationFrame     || 
+                  function(/* function */ callback, /* DOMElement */ element){
+                    window.setTimeout(callback, 1000 / 60);
+                  };
+    })();
+
 	window.onload = function(e) {
 		var draggable = document.getElementById("dragged");
 		var viewPort = document.querySelector(".viewport");
 
 		for (var i = 0; i < 50; i++) {
 			var el = document.createElement("div");
-			el.className = "item";
+			el.className = "item " + ((i % 2  == 0) ? "even" : "odd");
 			el.innerText = i + 1;
 			draggable.appendChild(el);
 		}
@@ -13,9 +24,12 @@
 		draggable.addEventListener('mousedown', tap);
 		draggable.addEventListener('mousemove', drag);
 		draggable.addEventListener('mouseup', release);
-		var offset = 0, reference = 0,min = 0;
+		var offset = 0, reference = 0,min = 0, frame;
 		var pressed = false;
 		var max = viewPort.scrollWidth;//need to work on this
+		var velocity, amplitude, target;
+		var timestamp, ticker;
+		var timeConstanct = 325;
 
 		function scroll(x) {
 			offset = (x > max) ? max : (x < min) ? min : x;
@@ -25,18 +39,63 @@
 		function tap(e) {
 			pressed = true;
 			reference = e.clientX;
+
+			velocity = amplitude = 0;
+			frame = offset;
+			timestamp = Date.now();
+			clearInterval(ticker);
+			ticker = setInterval(track, 100);
+
 			e.preventDefault();
-			e.stopPropogation();
+			e.stopPropagation();
 
 			return false;
 		}
 
+		function track() {
+			var now, elapsed, delta, v;
+
+			now = Date.now();
+			elapsed = now - timestamp;
+			timestamp = now;
+			delta = offset - frame;
+			frame = offset;
+
+			v = 1000 * delta / (1 + elapsed);
+			velocity = 0.8 * v + 0.2 * velocity;
+		}
+
 		function release(e) {
 			pressed = false;
+
+			clearInterval(ticker);
+
+			if (velocity > 10 || velocity < -10) {
+				amplitude = 0.8 * velocity;
+				target = Math.round(offset + amplitude);
+				timestamp = Date.now();
+				requestAnimationFrame(autoScroll);
+			}
+
 			e.preventDefault();
-			e.stopPropogation();
+			e.stopPropagation();
 
 			return false;
+		}
+
+		function autoScroll() {
+			var elapsed, delta;
+
+			if (amplitude) {
+				elapsed = Date.now() - timestamp;
+				delta = -amplitude * Math.exp(-elapsed / timeConstanct);
+				if (delta > 0.5 || delta < -0.5) {
+					scroll(target + delta);
+					requestAnimationFrame(autoScroll);
+				} else {
+					scroll(target);
+				}
+			}
 		}
 
 		function drag(e) {
@@ -52,7 +111,7 @@
 			}
 
 			e.preventDefault();
-			e.stopPropogation();
+			e.stopPropagation();
 
 			return false;
 		}
